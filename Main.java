88057e363320fa91926e287c8dc97aaf632a3d23
Oskar <95188470+Tier1Coder@users.noreply.github.com>
2024-08-22 20:06:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,10 @@ import java.io.IOException;
 
 public class Main{
     public static void main(String[] args) {
-        // "C:\\Users\\219\\Desktop\\Python311"      C:\Users\akasi\Desktop\matura
-        // "C:\\Users\\219\\Desktop\\folder"         C:\Users\akasi\Desktop\ttt
-        String srcDirectory = "C:\\Users\\akasi\\Desktop\\test_to_copy"; // ścieżka źródłowa
-        String destDirectory = "C:\\Users\\akasi\\Desktop\\ttt"; // ścieżka docelowa
-        int nThreads = 2; // liczbą wątków
-        int bufferSize = 8; // mnozona *1024 (bajtow) czyli 1 = 1KB
+        String srcDirectory = "C:\\source\\path";
+        String destDirectory = "C:\\destination\\path";
+        int nThreads = 2; 
+        int bufferSize = 8; // multiplied by 1024 = 1KB
 
         try {
             MultithreadedFileCopy.copyFiles(srcDirectory, destDirectory, nThreads, bufferSize);
@@ -18,12 +16,13 @@ public class Main{
     }
 }
 
+// times:
 
-// 1 watek 1024 buff - 42sek
-// 1 watek 4096 buff - 7,5 sek
+// 1 thread 1024 buff - 42sek
+// 1 thread 4096 buff - 7,5 sek
 
-// 2 watki 1024 buff - 7,5sek
-// 2 watki 8 buff - 5,1 sek
+// 2 threads 1024 buff - 7,5sek
+// 2 threads 8 buff - 5,1 sek
 
-// 4 watki 1024 buff - 5,3sek
-// 4 watki 4096 buff - 4,6 sek
+// 4 threads 1024 buff - 5,3sek
+// 4 thread 4096 buff - 4,6 sek
